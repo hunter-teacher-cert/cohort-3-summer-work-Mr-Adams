c6de5149cbeab8b4ba1efc8af29ed0443bcc4806
Mr-Adams <66141358+Mr-Adams@users.noreply.github.com>
2022-07-01 15:12:43 +0000

    revise NIM

--- programming/1/Nim.java
+++ programming/1/Nim.java
@@ -11,25 +11,37 @@ public class Nim
       Scanner input = new Scanner(System.in);
         Random rand = new Random();
         while(stones > 0) {
-        System.out.print("How many stones do you want to take first? (1-3): ");
+        System.out.print("How many stones do you want to take? (1-3): ");
         stonesTaken = input.nextInt();
+            if (stonesTaken>stones)//tests to make sure won't end with negative stones 
+            {stonesTaken=stones;
+             System.out.println("You cannot have negative stones.  Your guess has been changed to " +stones + " stones");
+            }
         while(stonesTaken > 3) {
-            System.out.print("Pick 1-3 stones please: ");
+            System.out.print("You can only pick 1-3 stones please revise your guess: ");
             stonesTaken = input.nextInt();
+            input.nextLine();
+            if (stonesTaken>stones)//tests to make sure won't end with negative stones 
+            {stonesTaken=stones;
+             System.out.println("You cannot have negative stones.  Your guess has been changed to " +stones + " stones");
+            }
         }
-            stones -= stonesTaken;
+            stones -= stonesTaken;//gives remaining stones
         if (stones <= 0) {
             System.out.println("Player wins");
+            input.nextLine();
             break;
         }
             System.out.println(stones + " Remaining");
         //computer turn
             stonesTaken = rand.nextInt(3)+1;
-            System.out.println("The computer took " + stonesTaken + " stones");
-            if (stonesTaken>stones) {
-                stonesTaken=stones;
+            if (stonesTaken>stones)//tests to make sure won't end with negative stones 
+            {stonesTaken=stones;
             }
-            stones -= stonesTaken;
+            System.out.println("The computer took " + stonesTaken + " stones");
+            
+            
+        stones -= stonesTaken;//gives remaining stones
             if (stones <= 0) {
             System.out.println("Computer wins");
                 break;
